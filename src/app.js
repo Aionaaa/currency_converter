ac9b0af2b7dc39861bb30b44ae7023861eb006bf
Iona Grant <ionagrant92@gmail.com>
2019-10-22 19:56:39 +0100

    no idea what to do

--- src/app.js
+++ src/app.js
@@ -4,10 +4,28 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-
+      currencyData: null
+    },
+    mounted() {
+      this.getCurrency();
     },
     computed: {
-      
+      euroToPound: function() {
+        return Math.round((this.EUR * this.GBP) * 100) / 100;
+      },
+      poundToEuro: function() {
+        return Math.round((this.GBP * this.EUR) * 100) / 100;
+      }
+    },
+    methods: {
+      getCurrency: function(){
+        fetch("https://api.exchangeratesapi.io/latest")
+        .then(response => response.json())
+        .then(data => this.currencyData = data.rates)
+      }
     }
   })
 })
+
+
+// Math.round(num * 100) / 100
